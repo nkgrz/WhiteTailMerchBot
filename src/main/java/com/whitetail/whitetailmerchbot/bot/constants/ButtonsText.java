44cbd55c201de8c4c0428f18b461ecedf823222d
nkgrz <is911n@icloud.com>
2024-05-17 16:22:10 +0300

    Add cost delivery and place order values

--- src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsText.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsText.java
@@ -10,4 +10,6 @@ public class ButtonsText {
     public static final String REVIEWS_BUTTON_TEXT = "Отзывы";
     public static final String CALCULATE_DELIVERY_TEXT = "Посчитать доставку";
     public static final String CHANGE_BASKET_TEXT = "Изменить корзину";
+    public static final int COST_DELIVERY = 400;
+    public static final String PLACE_ORDER = "Оформить заказ";
 }
