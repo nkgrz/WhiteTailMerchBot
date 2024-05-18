0447936e6b43ed940dedd536faa4c714e40f2f45
nkgrz <is911n@icloud.com>
2024-05-18 22:08:30 +0300

    Added setters

--- src/main/java/com/whitetail/whitetailmerchbot/entity/CartItem.java
+++ src/main/java/com/whitetail/whitetailmerchbot/entity/CartItem.java
@@ -1,6 +1,7 @@
 package com.whitetail.whitetailmerchbot.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "cart_items")
@@ -12,16 +13,19 @@ public class CartItem {
     private Long cartItemId;
 
     @Getter
+    @Setter
     @ManyToOne
     @JoinColumn(name = "chat_id", referencedColumnName = "chat_id")
     private User user;
 
     @Getter
+    @Setter
     @ManyToOne
     @JoinColumn(name = "product_id", referencedColumnName = "product_id")
     private Product product;
 
     @Getter
+    @Setter
     @Column(name = "quantity")
     private Integer quantity;
 }
