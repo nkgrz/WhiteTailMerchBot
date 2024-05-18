d9822c950ead5bb28cc012fb3e7573b2a3a3ab05
nkgrz <is911n@icloud.com>
2024-05-18 22:03:04 +0300

    Added callbacks for buttons

--- src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsCallback.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsCallback.java
@@ -10,5 +10,8 @@ public class ButtonsCallback {
         public static final String CALCULATE_DELIVERY_CALLBACK = "calculate_delivery";
         public static final String CHANGE_BASKET_CALLBACK = "change_basket";
         public static final String PLACE_ORDER_CALLBACK = "place_order";
-        public static final String ADD_TO_CART_CALLBACK = " add_to_cart";
+        public static final String ADD_TO_CART_CALLBACK = "add_to_cart_";
+        public static final String PRODUCT_DETAILS_CALLBACK = "product_";
+        public static final String QUANTITY_CALLBACK = "quantity_";
+        public static final String LACK_OF_QUANTITY_CALLBACK = "lack_of_quantity";
 }
