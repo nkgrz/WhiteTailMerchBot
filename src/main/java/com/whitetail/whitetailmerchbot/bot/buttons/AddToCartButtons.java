929c2ca4b4b824c0ba2e894d903eb60baa116d0c
nkgrz <is911n@icloud.com>
2024-05-18 22:05:08 +0300

    Added a keyboard with numbers

--- src/main/java/com/whitetail/whitetailmerchbot/bot/buttons/AddToCartButtons.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/buttons/AddToCartButtons.java
@@ -8,20 +8,42 @@ import java.util.List;
 
 import static com.whitetail.whitetailmerchbot.bot.buttons.BackButtons.createBackButton;
 import static com.whitetail.whitetailmerchbot.bot.buttons.BackButtons.createMainButton;
-import static com.whitetail.whitetailmerchbot.bot.constants.ButtonsCallback.ADD_TO_CART_CALLBACK;
-import static com.whitetail.whitetailmerchbot.bot.constants.ButtonsCallback.CATALOG_CALLBACK;
-import static com.whitetail.whitetailmerchbot.bot.constants.ButtonsText.ADD_TO_CART;
+import static com.whitetail.whitetailmerchbot.bot.constants.ButtonsCallback.*;
+import static com.whitetail.whitetailmerchbot.bot.constants.ButtonsText.ADD_TO_CART_TEXT;
 
 public class AddToCartButtons {
-    public static InlineKeyboardMarkup createAddToCartButtons() {
+    public static InlineKeyboardMarkup createAddToCartButtons(int productId) {
         List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
 
         var buttonAdd = new InlineKeyboardButton();
-        buttonAdd.setText(ADD_TO_CART);
-        buttonAdd.setCallbackData(ADD_TO_CART_CALLBACK);
+        buttonAdd.setText(ADD_TO_CART_TEXT);
+        buttonAdd.setCallbackData(ADD_TO_CART_CALLBACK + productId);
 
         buttons.add(List.of(buttonAdd));
         buttons.add(List.of(createBackButton(CATALOG_CALLBACK), createMainButton()));
         return new InlineKeyboardMarkup(buttons);
+    }
+
+    public static InlineKeyboardMarkup createQuantityKeyboard(int productId, int quantity) {
+        List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i % 5 == 1) {
+                keyboardRows.add(new ArrayList<>());
+            }
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            if (quantity >= i) {
+                button.setText(i + " шт.");
+                button.setCallbackData(QUANTITY_CALLBACK + productId + "_" + i);
+            } else {
+                button.setText(" ");
+                button.setCallbackData(LACK_OF_QUANTITY_CALLBACK + productId + "_" + i);
+            }
+            keyboardRows.get(keyboardRows.size() - 1).add(button);
+
+        }
+        keyboardRows.add(List.of(createBackButton(CATALOG_CALLBACK), createMainButton()));
+        return new InlineKeyboardMarkup(keyboardRows);
     }
 }
