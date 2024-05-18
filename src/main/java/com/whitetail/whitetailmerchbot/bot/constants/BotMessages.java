7ec2e712e72dc4e2b0d86c38be0d81d7d8057f80
nkgrz <is911n@icloud.com>
2024-05-18 22:03:55 +0300

    Unused bot messages have been deleted, new ones have been added

--- src/main/java/com/whitetail/whitetailmerchbot/bot/constants/BotMessages.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/constants/BotMessages.java
@@ -3,10 +3,13 @@ package com.whitetail.whitetailmerchbot.bot.constants;
 public class BotMessages {
     public static final String WELCOME_MESSAGE = "Добро пожаловать в наш магазин!";
     public static final String CATALOG_MESSAGE = "Каталог товаров";
-    public static final String BASKET_MESSAGE = "Ваша корзина";
-    public static final String HISTORY_MESSAGE = "История заказов";
-    public static final String SUPPORT_MESSAGE = "Задать вопрос";
-    public static final String REVIEWS_MESSAGE = "Отзывы о нас";
     public static final String HELP_MESSAGE = "Вопросы о товарах задавать @annasalnikova1989\nЕсли что-то работает не так, как ожидалось – @storm256";
+    public static final String ADD_TO_CART_TEXT = "Выберите количество товара. Доступно: ";
+    public static final String CART_IS_EMPTY = "Корзина пуста";
+    public static final String ORDERS_IS_EMPTY = "Заказов пока не было";
+    public static final String MAIN_MENU_TEXT = "Главное меню";
+    public static final String PRODUCT_ADD_TO_CART_TEXT = "Товар успешно добавлен в корзину";
+    public static final String ERROR_ADD_TO_CART_TEXT = "Такого количества товара нет в наличии.\nПопробуйте еще раз.\nДоступно: ";
+
 }
 
