ba117d97541bab96c50219f55883c61560f43c72
nkgrz <is911n@icloud.com>
2024-05-19 17:12:07 +0300

    New callbacks have been added, old ones have been renamed and unnecessary ones have been removed

--- src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsCallback.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsCallback.java
@@ -2,16 +2,18 @@ package com.whitetail.whitetailmerchbot.bot.constants;
 
 public class ButtonsCallback {
         public static final String CATALOG_CALLBACK = "catalog";
-        public static final String CART_CALLBACK = "basket";
+        public static final String CART_CALLBACK = "cart";
         public static final String HISTORY_CALLBACK = "history";
         public static final String SUPPORT_URL = "https://t.me/storm256";
         public static final String REVIEWS_URL = "https://t.me/username";
         public static final String BACK_TO_MENU_CALLBACK = "back_to_menu";
         public static final String CALCULATE_DELIVERY_CALLBACK = "calculate_delivery";
-        public static final String CHANGE_BASKET_CALLBACK = "change_basket";
+        public static final String CHANGE_BASKET_CALLBACK = "changeBasket";
         public static final String PLACE_ORDER_CALLBACK = "place_order";
         public static final String ADD_TO_CART_CALLBACK = "add_to_cart_";
         public static final String PRODUCT_DETAILS_CALLBACK = "product_";
         public static final String QUANTITY_CALLBACK = "quantity_";
-        public static final String LACK_OF_QUANTITY_CALLBACK = "lack_of_quantity";
+        public static final String CHANGE_QUANTITY_FROM_CART_CALLBACK = "changeQuantity_";
+        public static final String CHANGE_QUANTITY_ITEM_FROM_CART_CALLBACK = "changeQuantityItem_";
+        public static final String BLANK_BUTTONS = "BLANK_BUTTONS";
 }
