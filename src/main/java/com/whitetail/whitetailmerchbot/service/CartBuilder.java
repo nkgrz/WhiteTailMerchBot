745183ff2a1763fe8cc5534e5506ed7fce02bd60
nkgrz <is911n@icloud.com>
2024-05-19 17:05:58 +0300

    Added several methods for different display of items in different parts of the basket, added a method for formatted price display

--- src/main/java/com/whitetail/whitetailmerchbot/service/CartBuilder.java
+++ src/main/java/com/whitetail/whitetailmerchbot/service/CartBuilder.java
@@ -3,16 +3,36 @@ package com.whitetail.whitetailmerchbot.service;
 import com.whitetail.whitetailmerchbot.entity.CartItem;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.List;
 
-import static com.whitetail.whitetailmerchbot.bot.buttons.BackButtons.createBackButton;
-import static com.whitetail.whitetailmerchbot.bot.constants.ButtonsText.*;
+import static com.whitetail.whitetailmerchbot.bot.constants.ButtonsText.COST_DELIVERY;
 
 public class CartBuilder {
-    public static String cartItemsToString(List<CartItem> cartItems) {
-        StringBuilder message = new StringBuilder();
-        message.append("Список ваших товаров в корзине:\n\n");
 
+    public static String cartItemsToString(List<CartItem> cartItems) {
+        CartSummary summary = buildCartItemsSummary(cartItems);
+        StringBuilder message = summary.messagePart();
+        BigDecimal totalPrice = summary.totalPrice();
+        int totalQuantity = summary.totalQuantity();
+
+        message.append("\nВсего товаров: ").append(totalQuantity).append(" шт.\n");
+        message.append("Стоимость: ").append(formatPrice(totalPrice));
+        message.append("Доставка: ").append(COST_DELIVERY).append("\n\n");
+        totalPrice = totalPrice.add(BigDecimal.valueOf(COST_DELIVERY));
+        message.append("Итого: ").append(formatPrice(totalPrice));
+        message.append("Все верно?");
+        return message.toString();
+    }
+
+    public static String formatPrice(BigDecimal price) {
+        DecimalFormat priceFormat = new DecimalFormat("##,###");
+        return priceFormat.format(price) + " руб.\n";
+    }
+
+    public static CartSummary buildCartItemsSummary(List<CartItem> cartItems) {
+        StringBuilder messagePart = new StringBuilder();
+        messagePart.append("Список ваших товаров в корзине:\n\n");
         BigDecimal totalPrice = BigDecimal.ZERO;
         int totalQuantity = 0;
 
@@ -22,20 +42,20 @@ public class CartBuilder {
             BigDecimal priceItem = item.getProduct().getPrice();
             BigDecimal itemTotalPrice = priceItem.multiply(BigDecimal.valueOf(quantityItem));
 
-            message.append(i + 1)
-                    .append(". ").append(cartItems.get(i).getProduct().getName())
+            messagePart.append(i + 1)
+                    .append(". ").append(item.getProduct().getName())
                     .append(" – ").append(quantityItem).append(" шт.")
-                    .append(" – ").append(itemTotalPrice).append(" руб.\n");
+                    .append(" – ").append(formatPrice(itemTotalPrice));
+
             totalPrice = totalPrice.add(itemTotalPrice);
             totalQuantity += quantityItem;
         }
+        return new CartSummary(messagePart, totalPrice, totalQuantity);
+    }
 
-        message.append("\nВсего товаров: ").append(totalQuantity).append(" шт.\n");
-        message.append("Стоимость: ").append(totalPrice).append(" руб.\n");
-        message.append("Доставка: ").append(COST_DELIVERY).append(" руб.\n\n");
-        totalPrice = totalPrice.add(BigDecimal.valueOf(COST_DELIVERY));
-        message.append("Итого: ").append(totalPrice).append(" руб.\n");
-        message.append("Все верно?");
+    public static String changeCartItemsToString(List<CartItem> cartItems) {
+        StringBuilder message = buildCartItemsSummary(cartItems).messagePart();
+        message.append("\nВыберите товар который хотите изменить: ");
         return message.toString();
     }
 }
