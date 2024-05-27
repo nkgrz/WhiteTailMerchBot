e0246279b105ac5ced7830022f625dc9a12bf8ea
nkgrz <is911n@icloud.com>
2024-05-27 16:25:59 +0300

    The code has been refactored

--- src/main/java/com/whitetail/whitetailmerchbot/bot/BotConfig.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/BotConfig.java
@@ -1,21 +1,19 @@
 package com.whitetail.whitetailmerchbot.bot;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Data // автоматическое создание конструкторов
+@Getter
+@Setter
 @Configuration
-@PropertySource("/application.yml")
+@ConfigurationProperties(prefix = "bot")
 public class BotConfig {
 
-    @Value("${bot.name}")
-    String botName;
+    String name;
 
-    @Value("${bot.token}")
     String token;
 
-    @Value("${bot.owner}")
-    long ownerId;
+    long owner;
 }
