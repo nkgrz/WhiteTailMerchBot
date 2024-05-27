4949bc5416af591294eb0b1a4d56daf8e352e559
nkgrz <is911n@icloud.com>
2024-05-27 16:28:41 +0300

    The code has been refactored

--- src/main/java/com/whitetail/whitetailmerchbot/bot/constants/BotMessages.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/constants/BotMessages.java
@@ -1,12 +1,12 @@
 package com.whitetail.whitetailmerchbot.bot.constants;
 
-public class BotMessages {
-    public static final String WELCOME_MESSAGE = "Добро пожаловать в наш магазин!";
-    public static final String CATALOG_MESSAGE = "Каталог товаров";
-    public static final String HELP_MESSAGE = "Вопросы о товарах задавать @annasalnikova1989\nЕсли что-то работает не так, как ожидалось – @storm256";
-    public static final String ADD_TO_CART_TEXT = "Выберите количество товара. Доступно: ";
-    public static final String CART_IS_EMPTY = "Корзина пуста";
-    public static final String ORDERS_IS_EMPTY = "Заказов пока не было";
-    public static final String MAIN_MENU_TEXT = "Главное меню";
-    public static final String PRODUCT_ADD_TO_CART_TEXT = "Товар успешно добавлен в корзину";
+public interface BotMessages {
+    String WELCOME_MESSAGE = "Добро пожаловать в наш магазин!";
+    String CATALOG_MESSAGE = "Каталог товаров";
+    String HELP_MESSAGE = "Вопросы о товарах задавать @annasalnikova1989\nЕсли что-то работает не так, как ожидалось – @storm256";
+    String ADD_TO_CART_TEXT = "Выберите количество товара. Доступно: ";
+    String CART_IS_EMPTY = "Корзина пуста";
+    String ORDERS_IS_EMPTY = "Заказов пока не было";
+    String MAIN_MENU_TEXT = "Главное меню";
+    String PRODUCT_ADD_TO_CART_TEXT = "Товар успешно добавлен в корзину";
 }
