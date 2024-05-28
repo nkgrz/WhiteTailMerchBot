8ce3792b1c487dceff07c71693db3462577edf41
nkgrz <is911n@icloud.com>
2024-05-29 01:29:27 +0300

    An unused file has been deleted

--- src/main/java/com/whitetail/whitetailmerchbot/service/CartSummary.java
+++
@@ -1,6 +0,0 @@
-package com.whitetail.whitetailmerchbot.service;
-
-import java.math.BigDecimal;
-
-public record CartSummary(StringBuilder messagePart, BigDecimal totalPrice, int totalQuantity) {
-}
