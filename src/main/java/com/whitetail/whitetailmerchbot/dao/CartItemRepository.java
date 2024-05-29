7c62bfed0cb3a416beca91c34b59cc601e17d818
nkgrz <is911n@icloud.com>
2024-05-29 10:26:02 +0300

    Solved TODO tasks have been deleted

--- src/main/java/com/whitetail/whitetailmerchbot/dao/CartItemRepository.java
+++ src/main/java/com/whitetail/whitetailmerchbot/dao/CartItemRepository.java
@@ -9,9 +9,3 @@ public interface CartItemRepository extends CrudRepository<CartItem, Long> {
     List<CartItem> findCartItemsByUserChatId(Long chatId);
     CartItem findCartItemByUserChatIdAndProductProductId(Long chatId, int productId);
 }
-
-// TODO 1.
-//  Получить все продукты по chat_id (для корзины)
-//  2. Изменить количество конкретного продукта в корзине
-//  (если больше 0, то оставить иначе удалить, использовать chat_id и product_id)
-//  3. delete (использовать chat_id и product_id)
