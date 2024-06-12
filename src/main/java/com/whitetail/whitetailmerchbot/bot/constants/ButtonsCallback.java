1f3e73448192b6ba224c92c9eda96fa9d15a487c
nkgrz <is911n@icloud.com>
2024-06-12 16:19:00 +0300

    An unused variable has been deleted

--- src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsCallback.java
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/constants/ButtonsCallback.java
@@ -7,7 +7,6 @@ public interface ButtonsCallback {
         String SUPPORT_URL = "https://t.me/storm256";
         String REVIEWS_URL = "https://t.me/username";
         String BACK_TO_MENU_CALLBACK = "back_to_menu";
-        String CALCULATE_DELIVERY_CALLBACK = "calculate_delivery";
         String CHANGE_BASKET_CALLBACK = "changeBasket";
         String PLACE_ORDER_CALLBACK = "place_order";
         String ADD_TO_CART_CALLBACK = "add_to_cart_";
