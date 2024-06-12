63ea95af0fec5148f507f305af3a191ba8b79055
nkgrz <is911n@icloud.com>
2024-06-12 16:20:38 +0300

    Added a class with other constants

---
+++ src/main/java/com/whitetail/whitetailmerchbot/bot/constants/OtherConstants.java
@@ -0,0 +1,9 @@
+package com.whitetail.whitetailmerchbot.bot.constants;
+
+public interface OtherConstants {
+    String ORDER_STATUS_CANCELED = "Отменен";
+    String ORDER_STATUS_UNPAID = "Заказ не оплачен";
+    String ORDER_STATUS_PAID = "Заказ оплачен";
+    int COST_DELIVERY = 400;
+    int MAX_NUMBER_ORDERS_PER_PAGE = 3;
+}
