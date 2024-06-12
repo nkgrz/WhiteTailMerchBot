f24cae746a40dc1d4a1842ea1a90d7f6324c4992
nkgrz <is911n@icloud.com>
2024-06-12 15:25:02 +0300

    Added a method that returns orders with a certain status and creation time

--- src/main/java/com/whitetail/whitetailmerchbot/dao/OrderRepository.java
+++ src/main/java/com/whitetail/whitetailmerchbot/dao/OrderRepository.java
@@ -3,11 +3,14 @@ package com.whitetail.whitetailmerchbot.dao;
 import com.whitetail.whitetailmerchbot.entity.Order;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
     Slice<Order> findOrdersByUserChatId(Long chatId, Pageable pageable);
     Order findOrderByOrderId(Long id);
+    List<Order> findOrdersByStatusAndOrderDateBefore(String status, Timestamp date);
 }
